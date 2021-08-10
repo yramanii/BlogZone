377da614c6e8a51c74661a784fbf8af515c5463f
yramanii <ramaniyash19@gmail.com>
2021-08-10 17:43:11 +0530

    testing done for models

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class createBlogTest(TestCase):
+    def setUp(self):
+        createBlog.objects.create(title='the secret', author='Rhonda Byrne')
+
+    def test(self):
+        obj1 = createBlog.objects.get(title='the secret', author='Rhonda Byrne')
+        self.assertEqual(obj1.title, 'the secret')
+        self.assertEqual(obj1.author, 'Rhonda Byrne')
+
+class contactTest(TestCase):
+    def setUp(self) -> None:
+        contact.objects.create(First_Name='yash', Last_Name='ramani', Email='yash@gmail.com')
+
+    def test(self):
+        obj2 = contact.objects.get(First_Name='yash', Last_Name='ramani', Email='yash@gmail.com')
+        self.assertEqual(obj2.First_Name, 'yash')
+        self.assertEqual(obj2.Last_Name, 'ramani')
+        self.assertEqual(obj2.Email, 'yash@gmail.com')
